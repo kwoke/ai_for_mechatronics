352428bd195304aca9f24930bf4730abe79ceaea
kwoke <kwoke@student.unimelb.edu.au>
2019-04-01 14:51:37 +1100

    Updated GeneralTools

--- GeneralTools/misc_fun.py
+++ GeneralTools/misc_fun.py
@@ -17,11 +17,12 @@ flags.DEFINE_string('TENSORFLOW_VERSION', '1.13.0', 'Version of TensorFlow for t
 flags.DEFINE_string('CUDA_VERSION', '9.0', 'Version of CUDA for the current project.')
 flags.DEFINE_string('CUDNN_VERSION', '7.0', 'Version of CuDNN for the current project.')
 
+
 # working directory info
-flags.DEFINE_string('DEFAULT_IN', 'C:/Users/richa/PycharmProjects/ai_mechatronics/Datasets/', 'Default input folder.')
-flags.DEFINE_string('DEFAULT_OUT', 'C:/Users/richa/PycharmProjects/ai_mechatronics/Results/', 'Default output folder.')
+flags.DEFINE_string('DEFAULT_IN', '/Volumes/GoogleDrive/My Drive/2019/MCEN90048 AI for Mechatronics/ai_for_mechatronics/Datasets/', 'Default input folder.')
+flags.DEFINE_string('DEFAULT_OUT', '/Volumes/GoogleDrive/My Drive/2019/MCEN90048 AI for Mechatronics/ai_for_mechatronics/Results/', 'Default output folder.')
 flags.DEFINE_string(
-    'DEFAULT_DOWNLOAD', 'C:/Users/richa/PycharmProjects/ai_mechatronics/Datasets/',
+    'DEFAULT_DOWNLOAD', '/Volumes/GoogleDrive/My Drive/2019/MCEN90048 AI for Mechatronics/ai_for_mechatronics/Datasets/',
     'Default folder for downloading large datasets.')
 flags.DEFINE_string('INCEPTION_V1', None, 'Folder that stores Inception v1 model.')
 
